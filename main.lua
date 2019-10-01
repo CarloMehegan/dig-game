fbef116c78ae7e92304e60e51e393bee4ef22673
Carlo <CarloMehegan@gmail.com>
2019-10-01 12:55:23 -0400

    nothing is fixed but tile is a proper class i think

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@
 
 tiles = {}
 function love.load()
+  -- love.graphics.setBackgroundColor(0.2, 0.2, 0.2)
   w = 30
   h = 20
   love.window.setMode(40*w, 40*h)
@@ -11,7 +12,7 @@ function love.load()
     tiles[x] = {}
     for y=1,h do
       -- tiles[x][y] = Tile(x*61+100,y*61+300,60,60)
-      tiles[x][y] = Tile( x, y, 40, 40)
+      tiles[x][y] = Tile:Create( {x = x, y = y, w = 40, h = 40} )
     end
   end
   tiles[w/2][h/2].broken = true
@@ -25,7 +26,7 @@ end
 function love.draw()
   for x=1,w do
     for y=1,h do
-      tiles[x][y]:Draw(.71,.40,.11)
+      tiles[x][y]:Draw()
     end
   end
   boy:Draw()
@@ -41,8 +42,11 @@ function Boy(x,y)
   boy.h = 16
   boy.img = love.graphics.newImage('diggin boy.png')
 
+  boy.leftpressed = false
   function boy:Update(dt)
-
+    if onePressKey('a', self.leftpressed) then
+      self.x = self.x + 10
+    end
   end
 
   function boy:Draw()
@@ -53,15 +57,14 @@ function Boy(x,y)
   return boy
 end
 
-function Tile(x,y,w,h)
-  local tile = {}
+Tile = {
+  x, y, w, h, c
+}
+function Tile:Create(tile)
+  local tile = tile or {}
 
-  tile.x = x
-  tile.y = y
-  tile.w = w
-  tile.h = h
-
-  tile.a = love.math.random(50,100)/100.0
+  tile.c = {.71,.40,.11,love.math.random(50,100)/100.0}
+  -- tile.a = love.math.random(50,100)/100.0
 
   tile.broken = false
 
@@ -69,13 +72,15 @@ function Tile(x,y,w,h)
 
   end
 
-  function tile:Draw(r,g,b)
+  function tile:Draw()
     if self.broken == false then
-      love.graphics.setColor(r, g, b, self.a)
+      love.graphics.setColor(self.c)
       love.graphics.rectangle("fill", (self.x-1)*40, (self.y-1)*40, self.w, self.h)
     end
   end
 
+  setmetatable(tile, self)
+  self.__index = self
   return tile
 end
 
@@ -83,4 +88,16 @@ end
 
 function love.keypressed(key, scancode, isrepeat)
   -- body...
+end
+
+function onePressKey(key, bool)
+  if love.keyboard.isDown(key) and bool == false then
+    bool = true
+    return true
+  elseif love.keyboard.isDown(key) == false and bool == true then
+    bool = false
+    return false
+  elseif love.keyboard.isDown(key) == true and bool == true then
+    return false
+  end
 end
